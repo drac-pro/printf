295996ee277f07870734a95cbda09774393056d6
Therese-Claire <awonguagabi@gmail.com>
2023-10-17 13:31:41 -0700

    added function to converter_2.c to handle the specifier u

--- converter_2.c
+++ converter_2.c
@@ -35,4 +35,41 @@ int print_binary(unsigned int n)
 	count++;
 	return (count);
 }
+/**
+ * print_u - function prints unsigned interger
+ * @args: unsigned number to print
+ *
+ * Return: number of bytes
+ */
+int print_u(va_list args)
+{
+	unsigned int num, i, j, temp, num_digits = 0, digit;
+	int count = 0;
 
+	num = va_arg(args, unsigned int);
+	if (num == 0)
+		count += _putchar('0');
+	else
+	{
+		temp = num;
+		while (temp > 0)
+		{
+			num_digits++;
+			temp /= 10;
+		}
+		for (i = num_digits - 1; i > 0; i--)
+		{
+			unsigned int power = 1;
+
+			for (j = 0; j < i; j++)
+			{
+				power *= 10;
+			}
+			digit = num / power;
+			count += _putchar(digit + '0');
+			num -= digit * power;
+		}
+	}
+
+	return (count);
+}
