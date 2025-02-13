a9dd2edbed81af32793199890935ec6cea520c50
Darius Tohtin <dariustohtin@gmail.com>
2025-02-13 16:46:13 +0100

    handled case when format specifier is unknown

--- handle_error.c
+++ handle_error.c
@@ -8,22 +8,8 @@
  */
 int str_error(char c)
 {
-	if (c == ' ')
-	{
-		_putchar('%');
-		_putchar(c);
-		return (2);
-	}
-	if (c == '\n')
-	{
-		_putchar('%');
-		_putchar('\n');
-		return (2);
-	}
-	else
-	{
-		_putchar('%');
-		return (1);
-	}
-	return (1);
+	/* handles unknown format specifier */
+	_putchar('%');
+	_putchar(c);
+	return (2);
 }
